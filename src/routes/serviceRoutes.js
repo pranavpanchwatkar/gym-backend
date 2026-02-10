56a5fe35c556665a1a4b7392948f788148266727
prajwal <prajwaltijare2020@gmail.com>
2026-02-10 11:35:54 +0530

    updated code

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import { createService, deleteServiceById, getServices, getServiceById, updateService } from '../controller/servicesController.js';
-
 import verify, { isAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/create', verify, isAdmin, createService);
 router.get('/getservices', getServices);
+
+// ⭐ PUBLIC ROUTE (FRONTEND USE KAREGA)
+router.get('/:id', getServiceById);
+
+// 🔒 ADMIN ROUTE (dashboard ke liye)
 router.get('/getservicebyid/:id', verify, isAdmin, getServiceById);
+
 router.put('/update/:id', verify, isAdmin, updateService);
 router.delete('/delete/:id', verify, isAdmin, deleteServiceById);
 
